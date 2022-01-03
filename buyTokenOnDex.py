a5ba2d8ed78d62d039fa21fdb74d75646f6d4ac0
soksay <34276672+soksay@users.noreply.github.com>
2022-01-03 02:08:10 +0100

    Add files via upload

--- buyTokenOnDex.py
+++ buyTokenOnDex.py
@@ -2,18 +2,19 @@ import pandas as pd
 from web3 import Web3
 import config
 import time
+import datetime
 
 def choice_dex():
 
     df = pd.read_excel("dex_parameters.xlsx")
     liste_dex = df["Dex"].values
 
-    print("Choisissez un DEX parmi :", liste_dex)
-    choiceDex = input("Entrez votre choix ici : ")
+    print("Choose a dex in the following list :", liste_dex)
+    choiceDex = input("Type your choice here : ")
 
     validation = None
     for j in range(0, len(liste_dex)):
-        if choiceDex == liste_dex[j]:
+        if choiceDex.lower() == liste_dex[j].lower():
             validation = True
             break
         else:
@@ -37,17 +38,17 @@ def choice_dex():
         balance = web3.eth.get_balance(config.sender_address)
         humanReadableBalance = web3.fromWei(balance, 'ether')
 
-        print("Vérification de la connexion à la blockchain ", DexChosen["Blockchain"].values[0], " : ", web3.isConnected())
-        print("Le token natif de la blockchain est ", nameNativeContract, "(", symbolNativeContract,
-              ") son addresse est : ", nativeTokenAddress)
+        print("Verifying connection ", DexChosen["Blockchain"].values[0], " : ", web3.isConnected())
+        print("Native token of the blockchain is ", nameNativeContract, "(", symbolNativeContract,
+              "), address is : ", nativeTokenAddress)
 
         contract = web3.eth.contract(
             address=web3.toChecksumAddress(DexChosen["RouterAddress"].values[0]),
             abi=DexChosen["RouterAddressAbi"].values[0])
 
-        print("Vous avez choisi le DEX ", DexChosen["Dex"].values[0])
+        print("You have chosen DEX : ", DexChosen["Dex"].values[0])
     else:
-        print("La valeur entrée est incorrecte. Veuillez saisir un DEX qui figure dans la liste", liste_dex )
+        print("Incorrect value. Please select a DEX in the following list ", liste_dex )
         print("")
         choice_dex()
 
@@ -60,42 +61,52 @@ def param_tx():
         global gasPrice
         global gasPriceGwei
         choiceGasPrice = None
-        print("Le gas price actuel est de :",  web3.fromWei(web3.eth._gas_price(),"gwei") ,"gwei sur la blockchain",
+        print("Current gas price is :",  web3.fromWei(web3.eth._gas_price(),"gwei") ,"gwei on the blockchain",
             DexChosen["Blockchain"].values[0])
 
-        choiceGasPrice = input("Saisissez le multiplicateur à appliquer au gas price pour votre transaction : ")
+        choiceGasPrice = input(("Please select a multiplier on the current gas price : "))
         gasPrice = web3.eth._gas_price()
         gasPriceGwei = web3.fromWei(gasPrice, "gwei")
-        finalGasPrice = gasPriceGwei * int(choiceGasPrice)
-        print("Le gas price de votre transaction sera de ", finalGasPrice, "gwei.")
+        finalGasPrice = float(gasPriceGwei) * float(choiceGasPrice)
+        print("Timestamp : ", datetime.datetime.now(), " - Gas price of your transaction will be ", finalGasPrice, "gwei.")
 
     def setAmountTokenToSpend():
         global amountTokenToSpend
-        print("Vous détenez ", humanReadableBalance, " ", symbolNativeContract, " dans votre wallet")
-        print("Combien de ", nameNativeContract, "(", symbolNativeContract, ") voulez-vous dépenser ?")
-        amountTokenToSpend = input("Saisissez le montant ici : ")
+        print("You have ", humanReadableBalance, " ", symbolNativeContract, " in your wallet")
+        print("How many ", nameNativeContract, "(", symbolNativeContract, ") do you want to spend ?")
+        amountTokenToSpend = input("Select the amount here : ")
         if float(amountTokenToSpend) >= float(humanReadableBalance):
-            print("La valeur entrée doit être inférieure à ", humanReadableBalance )
+            print("The value selected must be inferior to : ", humanReadableBalance )
             print("")
             setAmountTokenToSpend()
 
     def setSlippage():
-        slippage = float(input("Quel est le slippage souhaité (entre 0.1 et 49): "))
-        if 0.1 <= slippage <= 49:
+        slippage = float(input("Select the slippage (between 0.1 and 99.9): "))
+        if 0.1 <= slippage <= 99.9:
             global slippage_percent
             slippage_percent = slippage / 100
         else:
-            print("Le slippage doit être compris entre 0.1 et 49.")
+            print("Slippage must be set between 0.1 and 99.9. Retry.")
             print("")
             setSlippage()
 
-    ### Appelez les fonction paramètres
+    def setTokenToBuy():
+        global tokenToBuy
+        print("/!\ the transaction will be sent directly after this step /!\ ")
+        tokenAddressChosen = input("Type the address of the token you want to buy : ")
+        if Web3.isAddress(tokenAddressChosen):
+            tokenToBuy = Web3.toChecksumAddress(tokenAddressChosen)
+        else:
+            print("The token address that you typed is not a token address. Retry.")
+            print("")
+            setTokenToBuy()
+
+    ### Call parameters functions
     gasPriceChoice()
     setAmountTokenToSpend()
     setSlippage()
+    setTokenToBuy()
 
-    global tokenToBuy
-    tokenToBuy = web3.toChecksumAddress(input("Entrez l'addresse du token que vous souhaitez acheter: "))
     # Cake = 0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82 - BsC
     # Solar = 0x6bD193Ee6D2104F14F94E2cA6efefae561A4334B - Moonriver
     # Joe = 0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd - Avalanche
@@ -110,8 +121,8 @@ def send_tx():
     nonce = web3.eth.get_transaction_count(config.sender_address)
     gasPrice = web3.eth._gas_price()
     gasPriceGwei = web3.fromWei(gasPrice, "gwei")
-    finalGasPrice = gasPriceGwei * int(choiceGasPrice)
-    print("UPDATE : Le gas price de votre transaction sera de : ", finalGasPrice)
+    finalGasPrice = float(gasPriceGwei) * float(choiceGasPrice)
+    print("UPDATE Timestamp : ", datetime.datetime.now(), " - Gas price of your transaction is", finalGasPrice, "gwei.")
 
     def setBuyAmount():
         global buy_amount
@@ -131,8 +142,8 @@ def send_tx():
     humanReadable_buyamount = float(web3.fromWei(buy_amount[1], "ether"))
     global minimumTokenReceived
     minimumTokenReceived = humanReadable_buyamount * (1 - slippage_percent)
-    print("Vous recevrez ", humanReadable_buyamount, " tokens (", minimumTokenReceived, " au minimum) pour",
-          amountTokenToSpend, " ", nameNativeContract, "(", symbolNativeContract, ") dépensés")
+    print("You will receive ", humanReadable_buyamount, " tokens (", minimumTokenReceived, " at the minimum) for",
+          amountTokenToSpend, " ", nameNativeContract, "(", symbolNativeContract, ") spent")
 
     if DexChosen["Dex"].values[0] == "traderjoe":
         tx = contract.functions.swapExactAVAXForTokens(
@@ -145,7 +156,7 @@ def send_tx():
             'from': config.sender_address,
             'value': web3.toWei(amountTokenToSpend, 'ether'),
             # This is the token BNB amount you want to swap from
-            'gas': 26000000,
+            'gas': 1000000,
             'gasPrice': web3.toWei(finalGasPrice, 'gwei'),
             'nonce': nonce,
         })
@@ -167,19 +178,25 @@ def send_tx():
 
     signed_txn = web3.eth.account.sign_transaction(tx, private_key=config.private)
     tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
-    print(web3.toHex(tx_token))
+    print("Transaction ID : ",web3.toHex(tx_token))
     beginning_tx = time.time()
     tx_receipt = web3.eth.wait_for_transaction_receipt(tx_token)
     ending_tx = time.time()
-    temps_tx = ending_tx - beginning_tx
-    print("Temps pour exécuter la transaction : ", temps_tx, "secondes.")
-    print("Statut de la tx : ", tx_receipt.status)
-    again = input("Envoyer la même transaction ? Ecrivez ok si oui : ")
+    time_tx = ending_tx - beginning_tx
+    print("Time to execute the transaction : ", time_tx, "seconds.")
+
+    if tx_receipt.status == 1:
+        tx_status = "sent"
+    else:
+        tx_status = "not sent"
+
+    print("Status of the transaction : ", tx_status)
+    again = input("Send same transaction ? type 'ok' if you want : ")
     if again.lower() == "ok":
         send_tx()
     else:
         print("")
-        print("Fin du programme merci d'avoir utiliser, vous pouvez faire un don à l'adresse : 0xd4d1D355B967c6b67F29Fa68F7A3CCcaeF82E90c :) ")
+        print("End of the script, send donations if you feel like it : 0xf444955E4dC892198E8a733ffCf08aaA13Bea096 :) ")
         exit()
 
 def main():
