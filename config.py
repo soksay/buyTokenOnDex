17e489a454492246d649f51c86b9111adf116a1f
soksay <34276672+soksay@users.noreply.github.com>
2022-01-10 00:25:58 +0100

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-from web3 import Web3
-
-private = "9fe6c081baa8bb046125f9c809236b6e1860a802bb41fa5e003985e2bc692f8c"
-sender_address = Web3.toChecksumAddress("0x73d2DC596dbcB0FD26a4B8388Fa440334054Ce19")
-
